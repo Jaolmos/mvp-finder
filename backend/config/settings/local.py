7098ef6a1e8baea897933b4f7b3bf0fc67f1ef3b
Jose Antonio Olmos Martinez <jose.a.olmosmartinez@gmail.com>
2026-01-22 19:22:36 +0100

    fix: corregir nombre de base de datos en settings local

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -13,7 +13,7 @@ if os.environ.get('POSTGRES_HOST'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_DB', 'reddit_mvp_finder'),
+            'NAME': os.environ.get('POSTGRES_DB', 'mvp_finder'),
             'USER': os.environ.get('POSTGRES_USER', 'postgres'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
             'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
